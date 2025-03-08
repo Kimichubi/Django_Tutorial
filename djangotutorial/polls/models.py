d67fd916df8f2b29d81d5688e4ffdec6d77ac9c7
Kimichubi <gabrielpoyta@outlook.com>
2025-03-08 12:56:38 -0300

    Implementando testes com DJANGO e tambem implementando a nova função na CLASSE QUESTION que retorna se a QUESTION foi publicada recentemente.

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -1,10 +1,7 @@
-from datetime import datetime
-from time import timezone
+import datetime
 
 from django.db import models
-
-# Create your models here.
-from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -13,6 +10,10 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
